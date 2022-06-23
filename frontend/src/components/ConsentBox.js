ed690825126ec0619568f7e1336977b7ecd50c04
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-06-23 12:03:33 -0400

    Added the Consent box

--- frontend/src/components/ConsentBox.js
+++ frontend/src/components/ConsentBox.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import useState from 'react';
+import React, {useState} from 'react';
+import { Button } from 'semantic-ui-react'
+import { useHistory } from 'react-router-dom';
 
 function ConsentBox() {
     const [show, setShow] = useState(true);
+    const history=useHistory();
     // const [box, setBox] = useState(false);
 
     console.log('hi')
+
+    function handleClick(){
+      history.push("/Home") 
+    }
    
     return (
       <div>
-        <img src='https://imgur.com/a/Wp1ETd2' alt = 'mark made this'/>
+        <img src='https://i.imgur.com/C8zv9Wk.png' alt = 'mark made this'/>
         <ul>
             <li>Behave yourself</li>
             <li>Post must be in English</li>
             <li>NSFW guidelines</li>
             <li>No abusive or toxic behavior</li>
         </ul>
-        <button>I agree</button>
+          <Button onClick={handleClick}>
+           <p>I Agree!</p>
+          </Button>
       </div>
     );
   }
